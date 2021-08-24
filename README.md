f3fd741f7106a65dc48986c27eebbabb608c9b99
jathu <jathu.satkunarajah@gmail.com>
2021-08-24 11:00:16 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,53 @@
+![with-logger](github-banner.png)
+
+![Workflow status](https://github.com/jathu/with-logger/actions/workflows/publish_master_to_npm.yml/badge.svg)
+
+## Installation
+
+[npmjs.com/package/with-logger](https://www.npmjs.com/package/with-logger)
+
+```bash
+# Yarn
+$ yarn add with-logger
+
+# NPM
+$ npm install with-logger
+```
+
+## Usage
+
+Use it like `console`:
+
+```typescript
+import { logger } from "with-logger";
+
+const metadata = { 
+    userId: "2e01d247-1dc1-4d39-a287-f8a069544d43",
+    location: "San Francisco, CA"
+};
+
+logger.info("👋 Hello World");
+// 2021-01-01 12:34:56.416 info : 👋 Hello World
+
+logger.debug("User metadata", metadata);
+// 2021-01-01 12:34:56.416 debug : User metadata {
+//   userId: "2e01d247-1dc1-4d39-a287-f8a069544d43",
+//   location: "San Francisco, CA"
+// }
+
+logger.with({ userId: metadata.userId  }).info("User changed location to", newLocation);
+// 2021-01-01 12:34:56.416 info [userId=2e01d247-1dc1-4d39-a287-f8a069544d43] : User changed location to Menlo Park, CA
+
+logger.with({ metadata }).warn("Failed to sync user location");
+// 2021-01-01 12:34:56.416 info [userId=2e01d247-1dc1-4d39-a287-f8a069544d43] [location=San Francisco, CA] : Failed to sync user location
+
+try {
+    perform();
+} catch (error) {
+    logger.with({ session }).error(error);
+}
+// 2021-01-01 12:34:56.416 error [id=6033bda3-1a63-44a9-97f1-e15e097c98c0] : Unable to reach server
+```
+
+------
+August 2021 - Toronto
---
+++ src/index.ts
@@ -0,0 +1,86 @@
+interface Logger {
+  /**
+   * A logger instance. Similar to console logger with some more context.
+   *
+   * @param message log message.
+   * @param optionalParams objects with which to replace substitution strings
+   * within message. This gives you additional control over the format of the
+   * output.
+   */
+  (message?: any, ...optionalParams: any[]): void;
+}
+
+type Level = "debug" | "info" | "warn" | "error";
+
+const ansi = (code: number, message: string): string =>
+  ["\x1b[", code, "m", message, "\x1b[0m"].join("");
+
+const zeroPad = (value: number, limit = 10) =>
+  value < limit ? `0${value}` : value.toString();
+
+const formattedTime = (now: Date): string =>
+  ansi(
+    2,
+    [
+      [now.getFullYear(), zeroPad(now.getMonth()), zeroPad(now.getDate())].join(
+        "-"
+      ),
+      [
+        [now.getHours(), now.getMinutes(), now.getSeconds()]
+          .map((digit: number) => zeroPad(digit))
+          .join(":"),
+        zeroPad(now.getMilliseconds(), 100),
+      ].join("."),
+    ].join(" ")
+  );
+
+const formattedLogLevel = (level: Level): string => {
+  switch (level) {
+    case "debug":
+      return ansi(32, "debug");
+    case "info":
+      return ansi(34, " info");
+    case "warn":
+      return ansi(33, " warn");
+    case "error":
+      return ansi(31, "error");
+  }
+};
+
+const formattedContext = (context: { [key: string]: any }): string | null =>
+  Object.keys(context)
+    .map((key) => ansi(2, `[${key}=${context[key]}]`))
+    .join(" ");
+
+const logPrefix = (level: Level, context: { [key: string]: any }): string =>
+  [
+    formattedTime(new Date()),
+    formattedLogLevel(level),
+    formattedContext(context),
+    ansi(2, ":"),
+  ]
+    .filter((component) => component)
+    .join(" ");
+
+const contextLogger = (context: {
+  [key: string]: any;
+}): { [key in Level]: Logger } => ({
+  debug: (message?: any, ...optionalParams: any[]): void =>
+    console.debug(logPrefix("debug", context), message, ...optionalParams),
+  info: (message?: any, ...optionalParams: any[]): void =>
+    console.info(logPrefix("info", context), message, ...optionalParams),
+  warn: (message?: any, ...optionalParams: any[]): void =>
+    console.warn(logPrefix("warn", context), message, ...optionalParams),
+  error: (message?: any, ...optionalParams: any[]): void =>
+    console.error(logPrefix("error", context), message, ...optionalParams),
+});
+
+/**
+ * Create a logger with some context. A context can be metadata relating to the
+ * log or some other useful information. It will be logged inline with the log
+ * line.
+ *
+ * @param context an object containing the metadata.
+ * @returns an object of loggers.
+ */
+export const logger = { with: contextLogger, ...contextLogger({}) };
